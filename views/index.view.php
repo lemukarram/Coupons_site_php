1796ffbf450a5374ac5652de06a29f44ec6cb923
Mukarram <mshah@rsg.one>
2026-04-12 00:18:20 +0300

    homepage section reorder

--- views/index.view.php
+++ views/index.view.php
@@ -5,11 +5,12 @@
 <?php require './sections/home.php'; ?>
 
 <!-- CONTENT -->
+ <?php require './sections/featured-stores.php'; ?>
 <?php require './sections/exclusive-coupons.php'; ?>
-<?php require './sections/featured-coupons.php'; ?>
+<?php //require './sections/featured-coupons.php'; ?>
 <?php require './sections/featured-categories.php'; ?>
-<?php require './sections/featured-stores.php'; ?>
-<?php require './sections/categories.php'; ?>
+
+<?php //require './sections/categories.php'; ?>
 
 
 <!-- FOOTER -->
