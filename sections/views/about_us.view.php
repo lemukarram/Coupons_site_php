4f6df2c2bc4e0da435b8bbefc570d8a08ce5db06
Mukarram <mshah@rsg.one>
2026-04-12 11:26:43 +0300

    about us section update

--- sections/views/about_us.view.php
+++ sections/views/about_us.view.php
@@ -1,14 +1,14 @@
-<!-- Section: About Us (50/50) -->
+<!-- Section: About Us (70/30) -->
 <div class="uk-section uk-section-muted">
     <div class="uk-container">
-        <div class="uk-grid-large uk-child-width-1-2@m uk-flex-middle" uk-grid>
-            <div>
+        <div class="uk-grid-large uk-flex-middle" uk-grid>
+            <div class="uk-width-2-3@m">
                 <h2 class="uk-text-bold uk-text-primary"><?php echo echoOutput($section_about_us['section_title']); ?></h2>
                 <div class="uk-text-lead uk-margin-medium-bottom uk-text-secondary">
                     <?php echo parseCustomTags($section_about_us['section_description']); ?>
                 </div>
             </div>
-            <div class="uk-text-center">
+            <div class="uk-width-1-3@m uk-text-center">
                 <?php if($section_about_us['section_image']): ?>
                 <img src="<?php echo $urlPath->image($section_about_us['section_image']); ?>" alt="About Us Graphics" class="uk-responsive-width uk-border-rounded">
                 <?php else: ?>
